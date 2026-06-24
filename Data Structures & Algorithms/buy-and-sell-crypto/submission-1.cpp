d3d3a8d4650ab3ef1f82447e35d72171c8034503
Atharva Nanaware <atharvananaware876@gmail.com>
2026-06-24 17:10:29 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+
+        int buy = 0;
+        int sell = 1;
+
+        int maxiProfit = 0;
+
+        for(int i  = 0; i < n-1; i++){
+
+            // int profit = prices[sell] - prices[buy];
+
+            // if(profit >= 1){
+            //     maxiProfit = max(maxiProfit, profit);
+            // }
+
+            if(prices[buy] < prices[sell]){
+
+                int profit = prices[sell] - prices[buy];
+                maxiProfit = max(maxiProfit, profit);
+            }
+
+            else{
+                buy = sell;
+            }
+
+            sell++;
+        }
+
+        return maxiProfit;
+    }
+};
