44f2dd508fca32803a0001b59722ddfb8f1be0cd
Atharva Nanaware <atharvananaware876@gmail.com>
2026-07-02 22:49:19 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n = strs.size();
+
+        unordered_map<string, vector<string>> mpp;
+
+        for (int i = 0; i < n; i++) {
+            string temp = strs[i];
+
+            sort(temp.begin(), temp.end());
+
+            mpp[temp].push_back(strs[i]);   // key is pushed by mpp[temp]  and  vlaue is pushed by .push_back(strs[i])
+        }
+
+        /*
+        for(auto it: strs){
+
+            string temp = it;
+
+            sort(temp.begin(), temp.end());
+
+            mpp[temp].push_back(it);
+        }
+        */
+
+        vector<vector<string>> result;
+
+        for (auto it : mpp) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
