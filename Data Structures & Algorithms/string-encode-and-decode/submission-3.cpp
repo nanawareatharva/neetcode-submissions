69937c559840445b85b7db1cd32881b24e8331b0
Atharva Nanaware <atharvananaware876@gmail.com>
2026-07-08 16:57:25 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        string encodedString = "";
+
+        for(const auto &it: strs){
+
+            encodedString += to_string(it.size());
+            encodedString += "#";
+            encodedString += it;
+        }
+
+        return encodedString;
+    }
+
+    vector<string> decode(string s) {
+
+        vector<string> decodedString;
+
+        int n = s.length();
+        int i = 0;
+
+        while(i < n){
+
+            int j = i;
+
+            while(j < n && s[j] != '#'){
+                j++;
+            }
+
+            int len = stoi(s.substr(i));
+
+            string ans = s.substr(j+1, len);
+            decodedString.push_back(ans);
+
+            i = j + 1 + len;
+        }
+
+        return decodedString;
+    }
+};
