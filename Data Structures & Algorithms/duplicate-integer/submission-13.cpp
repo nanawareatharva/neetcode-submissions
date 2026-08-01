79579dee6cf87566cae03608b321b73439224415
Atharva Nanaware <atharvananaware876@gmail.com>
2026-08-01 16:00:36 +0530

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> st;
+
+        for (const auto &it: nums){
+
+            if(st.count(it) == 1){
+                return true;
+            }
+            st.insert(it);
+        }
+
+        return false;
+    }
+};
