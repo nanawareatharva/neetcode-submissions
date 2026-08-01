82777ccca0f47412fb587dd1d8a948574198e750
Atharva Nanaware <atharvananaware876@gmail.com>
2026-08-01 16:07:31 +0530

    Add: duplicate-integer - submission-15

---
+++ Data Structures & Algorithms/duplicate-integer/submission-15.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_map<int, int> mpp;
+
+        for(const auto &it: nums){
+            mpp[it]++;
+        }
+
+        for(const auto &it: mpp){
+            if(it.second != 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
