a6ec79b2563252ccbec9a7ee04aefd0230c472aa
Atharva Nanaware <atharvananaware876@gmail.com>
2026-08-01 17:09:32 +0530

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int,int> mpp;
+
+        for(const auto &it: nums){
+            mpp[it]++;
+        }
+
+        vector<pair<int,int>> freqVec;
+
+        for(const auto &it: mpp){
+            freqVec.push_back({it.second, it.first});
+        }
+
+        sort(freqVec.rbegin(), freqVec.rend());
+
+        vector<int> ans;
+
+        for(int i = 0; i < k; i++){
+            ans.push_back(freqVec[i].second);
+        }
+
+        return ans;
+    }
+};
